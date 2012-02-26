32e9fcb1302c052963c373ef9bf542e7307daa35
Matthew Nohr <nohrmr@gmail.com>
2012-02-26 13:51:23 -0600

    Added login information

--- racetrack/grails-app/conf/BootStrap.groovy
+++ racetrack/grails-app/conf/BootStrap.groovy
@@ -2,27 +2,44 @@ import grails.util.GrailsUtil
 import racetrack.Race
 import racetrack.Registration
 import racetrack.Runner
+import racetrack.User
 
 class BootStrap {
 
 	def init = { servletContext ->
 		switch(GrailsUtil.environment){
 			case "development":
-				//Runners
+			//Users
+				def admin = new User(login:"admin",
+				password:"wordpass",
+				role:"admin")
+				admin.save()
+				if(admin.hasErrors()){
+					println admin.errors
+				}
+				def jdoe = new User(login:"jdoe",
+						password:"password",
+						role:"user")
+				jdoe.save()
+				if(jdoe.hasErrors()){
+					println jdoe.errors
+				}
+
+			//Runners
 				def jane = new Runner(firstName:"Jane",
-				lastName:"Doe",
-				dateOfBirth:(new Date() - 365*30),
-				gender:"F",
-				address:"123 Main St",
-				city:"Goose",
-				state:"NC",
-				zipcode:"12345",
-				email:"jane@whereever.com")
+						lastName:"Doe",
+						dateOfBirth:(new Date() - 365*30),
+						gender:"F",
+						address:"123 Main St",
+						city:"Goose",
+						state:"NC",
+						zipcode:"12345",
+						email:"jane@whereever.com")
 				jane.save()
 				if(jane.hasErrors()){
 					println jane.errors
 				}
-				//Races
+			//Races
 				def trot = new Race(
 						name:"Turkey Trot",
 						startDate:(new Date() + 90),
@@ -36,7 +53,7 @@ class BootStrap {
 				if(trot.hasErrors()){
 					println trot.errors
 				}
-				//Registrations
+			//Registrations
 				def reg = new Registration(
 						paid:false,
 						runner:jane,
---
+++ racetrack/grails-app/taglib/racetrack/LoginTagLib.groovy
@@ -0,0 +1,5 @@
+package racetrack
+
+class LoginTagLib {
+
+}
