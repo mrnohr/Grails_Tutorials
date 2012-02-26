6beb85993a3e6d0d9e951abcfd74ec19153b584d
Matthew Nohr <nohrmr@gmail.com>
2012-02-26 14:00:33 -0600

    Only allow admins to view user list

--- racetrack/grails-app/controllers/racetrack/UserController.groovy
+++ racetrack/grails-app/controllers/racetrack/UserController.groovy
@@ -101,6 +101,25 @@ class UserController {
         }
     }
 	
+	def beforeInterceptor = [action:this.&auth,
+		except:[
+			'login',
+			'logout',
+			'authenticate'
+		]]
+	
+	def auth() {
+		if(!session.user) {
+			redirect(controller:"user", action:"login")
+			return false
+		}
+		if(!session.user.admin){
+			flash.message = "Tsk tsk—admins only"
+			redirect(controller:"race", action:"list")
+			return false
+		}
+	}
+	
 	def login = {}
 	def logout = {
 		flash.message = "Goodbye ${session.user.login}"
