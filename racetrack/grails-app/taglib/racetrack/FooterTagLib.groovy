b24fdfe12c386985725b39b74089389152b27f7c
Matthew Nohr <nohrmr@gmail.com>
2012-02-26 12:55:59 -0600

    Add cusom header / footer

---
+++ racetrack/grails-app/taglib/racetrack/FooterTagLib.groovy
@@ -0,0 +1,12 @@
+package racetrack
+
+class FooterTagLib {
+	def thisYear = {
+		out << new Date().format("yyyy")
+	}
+	
+	def copyright = {attrs, body ->
+		out << "&copy; " + attrs.startYear + " - "
+		out << thisYear() + " " + body()
+	}
+}
---
+++ racetrack/grails-app/views/layouts/_header.gsp
@@ -0,0 +1,6 @@
+<div id="header">
+	<p>
+		<a class="header-main" href="${resource(dir:'')}">RaceTrack</a>
+	</p>
+	<p class="header-sub">When's your next race?</p>
+</div>
