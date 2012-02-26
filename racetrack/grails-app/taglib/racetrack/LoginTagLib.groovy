3182c48162d800b97abdf997d71abf2d0953b374
Matthew Nohr <nohrmr@gmail.com>
2012-02-26 13:55:07 -0600

    Add login/logout button

--- racetrack/grails-app/taglib/racetrack/LoginTagLib.groovy
+++ racetrack/grails-app/taglib/racetrack/LoginTagLib.groovy
@@ -1,5 +1,14 @@
 package racetrack
 
 class LoginTagLib {
-
+	def loginControl = {
+		if(request.getSession(false) && session.user){
+			out << "Hello ${session.user.login} "
+			out << """[${link(action:"logout",
+		controller:"user"){"Logout"}}]"""
+		} else {
+			out << """[${link(action:"login",
+		controller:"user"){"Login"}}]"""
+		}
+	}
 }
--- racetrack/grails-app/views/layouts/_header.gsp
+++ racetrack/grails-app/views/layouts/_header.gsp
@@ -3,4 +3,7 @@
 		<a class="header-main" href="${resource(dir:'')}">RaceTrack</a>
 	</p>
 	<p class="header-sub">When's your next race?</p>
+	<div id="loginHeader">
+		<g:loginControl />
+	</div>
 </div>
