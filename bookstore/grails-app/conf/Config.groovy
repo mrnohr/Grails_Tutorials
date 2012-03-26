39965dcb97603ef6c8475bdc2a60fa9ba69f5135
Matt Nohr <matt.nohr@refactr.com>
2012-03-26 13:43:43 -0500

    run s2-quickstart

--- bookstore/grails-app/conf/Config.groovy
+++ bookstore/grails-app/conf/Config.groovy
@@ -91,3 +91,8 @@ log4j = {
            'org.hibernate',
            'net.sf.ehcache.hibernate'
 }
+
+// Added by the Spring Security Core plugin:
+grails.plugins.springsecurity.userLookup.userDomainClassName = 'bookstore.User'
+grails.plugins.springsecurity.userLookup.authorityJoinClassName = 'bookstore.UserRole'
+grails.plugins.springsecurity.authority.className = 'bookstore.Role'
